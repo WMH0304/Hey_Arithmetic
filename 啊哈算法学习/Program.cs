76686637abb6d16356d53cec1521374a3c6e6deb
TK <TK@DESKTOP-5CS10AR>
2020-09-14 10:22:37 +0800

    添加项目文件。

---
+++ 啊哈算法学习/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 啊哈算法学习
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
