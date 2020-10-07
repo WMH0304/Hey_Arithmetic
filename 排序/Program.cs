147e5bed94d05a7c99e8a7f23e94632e0b8c958f
TK <TK@DESKTOP-5CS10AR>
2020-10-07 18:59:09 +0800

    再次推送

--- 排序/Program.cs
+++ 排序/Program.cs
@@ -1,31 +1,126 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace 排序
 {
     class Program
     {
+
+        static List<int> list = new List<int>(10);
+        // static int[] vs = new int[10] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
         static void Main(string[] args)
         {
-            //随机生成 10 个数
-            List<int> list = new List<int>(10);
-            for (int i = 0; i < 10; i++)
-            {
-                Random random = new Random();
-                int it = random.Next(0, 10);
-                list.Add(it);  
-            }
-            #region 选择排序
 
-
-
-            #endregion
            
+            #region 快速排序
+            //检测时间
+            System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+            const int z = 1000000;
+            //生成随机数
+            int[] a = new int[z];
+            Random random = new Random();
+            // Parallel.For(0, z, (i) => { a[i] = random.Next(0, z); });
+            for (int i = 0; i < z; i++)
+            {
+                a[i] = random.Next(0, z);
+            }
+            watch.Start();//计时器开始。
+            QuickSort(a, 0, z - 1);
+            watch.Stop();//计时器结束。
+           
+            string time = watch.ElapsedMilliseconds.ToString();
+            Console.Write("排序所用时间 :" + " " + time + " ms");
+         
+            Console.ReadKey();
+            #endregion
+
+         
+        }
+
+        /*
+         * 快速排序思想类似于二分查找，都是确定一个中位数（排序里面叫做基准数）
+         * 排序步骤如下：
+         * 1.确定基准数
+         * 2.遍历数组将以基准数为界分为两个数组（也可以是多个）
+         * 3.然后分别对两个数组进行排序
+         * 4.最后合并数组
+         */
+
+            /// <summary>
+            /// 分组
+            /// </summary>
+            /// <param name="a"></param>
+            /// <param name="low"></param>
+            /// <param name="high"></param>
+        private static void QuickSort(int[] a, int low, int high)
+        {
+            if (low >= high)
+            {
+                return;
+            }
+           
+            //Task task = Task.Run(() => {  pivot = QuickSortOnce(a, low, high); });
+            //task.Wait();
+            
+
+            int pivot = QuickSortOnce(a, low, high);//输出每一次排序。
+
+            //对枢轴左端进行排序。
+            Task.Run(() =>
+            {
+                QuickSort(a, low, pivot - 1);
+            });
+
+            //对枢轴右端进行排序。
+            Task.Factory.StartNew(() =>
+            {
+                QuickSort(a, pivot + 1, high);
+            });
+            //QuickSort(a, low, pivot - 1);
+            //QuickSort(a, pivot + 1, high);
+        }
+        /// <summary>
+        /// 排序
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="low"></param>
+        /// <param name="high"></param>
+        /// <returns></returns>
+        private static int QuickSortOnce(int[] a, int low, int high)
+        {
+            //将首个元素作为枢轴。
+            int pivot = a[low];
+            int i = low, j = high;
+            while (i < j)
+            {
+                //从右往左，寻找首个小于povit的元素。
+                while (a[j] >= pivot && i < j)
+                {
+                    j--;
+                }
+                //执行到此，j一定指向从右端起首个小于或等于povit的元素。执行替换。
+                a[i] = a[j];
+                //从左往右，寻找首个大于povit的元素。
+                while (a[i] <= pivot && i < j)
+                {
+                    i++;
+                }
+                //执行到此，j一定指向从右端起首个大于或等于povit的元素。执行替换。
+                a[j] = a[i];
+            }
+            //退出while循环，执行至此，必定是i==j的情况。i（或j）指向的既是枢轴的位置，定位该趟排序的枢轴并将该位置返回。
+            a[i] = pivot;
+            return i;
 
 
 
-            Console.ReadLine();
+
+
+
         }
     }
 }
--- 递归/Program.cs
+++ 递归/Program.cs
@@ -7,7 +7,10 @@ namespace 递归
      * 啥是递归？
      * 1.递归指一种通过不断地调用自身从而缩减自身规模地算法（自己调用自己的函数）
      * 2.每个递归都有两个条件 递归条件 和返回条件（跳出条件）
-     * 3.递归作用在栈上（
+     * 3.递归作用在栈上
+     * 4.栈的操作： 压入和弹出
+     * 5.所以函数调用的实质都是在调用栈
+     * 
      * 我是一个递归
      * 沉睡在栈中
      * 你的温柔唤醒了我
@@ -16,7 +19,7 @@ namespace 递归
      * 如果条件为true则结束询问并打印家的字符
      * 否则继续询问
      * 直至宇宙消亡
-     * ）
+     * 
      * 
      * 他能干啥？
      * 和普通的循环有什么区别？
@@ -27,29 +30,41 @@ namespace 递归
      */
     class Program
     {
+        static List<int> vs = new List<int>();
+
         static void Main(string[] args)
         {
             Random random = new Random();
-            List<int> vs = new List<int>();
-            for (int i = 0; i < 10; i++)
+            
+            for (int i = 0; i < 5; i++)
             {
                 vs.Add(random.Next(0, 10));
             }
             vs.Add(6);
-            int gi=  Get_int(vs,0);//调用递归方法
-            Console.WriteLine(gi);
+            int gi = Get_int(0);//调用递归方法
+            Console.WriteLine("查找"+gi);
             //递归实现阶乘1
-            int gif = Get_int_factorial(5,1);
-            Console.WriteLine(gif);
+            int gif = Get_int_factorial(5, 1);
+            Console.WriteLine("阶乘" + gif);
+            //递归实现遍历数组累加
+            int gia = Get_int_accumulation(0,0);
+            Console.WriteLine("累加"+gia);
+            //找出最大数
+            int gibn = Get_int_big_num(0, 0);
+            Console.WriteLine("最大数"+ gibn);
+
+
+            Console.ReadKey();
 
         }
+      
         /// <summary>
         /// 递归查找
         /// </summary>
         /// <param name="vs"></param>
         /// <param name="j"></param>
         /// <returns></returns>
-        static int Get_int(List<int> vs,int j)
+        static int Get_int(int j)
         {
             /* 
              * 通过不断调用自身从而实现一个循环
@@ -59,24 +74,24 @@ namespace 递归
              * 使 j 自增 并将j 传入新地递归方法中
              * 
              */
-            if (vs[j] ==6)
+            if (vs[j] == 6)
             {
                 return vs[j];
             }
             j++;
-            return Get_int(vs, j++);
+            return Get_int(j++);
         }
-
+       
         /// <summary>
         /// 递归阶乘
         /// </summary>
         /// <param name="i">阶乘数</param>
         /// <param name="j">阶乘结果</param>
         /// <returns></returns>
-        static int Get_int_factorial(int i,int j)
+        static int Get_int_factorial(int i, int j)
         {
 
-            if (i==1)
+            if (i == 1)
             {
                 return j;
             }
@@ -84,8 +99,52 @@ namespace 递归
             {
                 j = j * i;
                 i--;
-                return Get_int_factorial(i,j);
+                return Get_int_factorial(i, j);
             }
         }
+       
+        /// <summary>
+        /// 累加
+        /// </summary>
+        /// <returns></returns>
+        static int Get_int_accumulation(int num,int i)
+        {
+           
+            num = num + vs[i];
+            //跳出条件
+            if (i == vs.Count-1)
+            {
+                return num;
+            }
+            i++;
+            return Get_int_accumulation(num,i);
+        }
+       
+        /// <summary>
+        /// 找出最大数
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <returns></returns>
+        static int Get_int_big_num(int i,int j)
+        {
+           
+            if (j<vs[i])
+            {
+                j = vs[i];
+            }
+
+            if (i ==vs.Count-1)
+            {
+                return j;
+            }
+            i++;
+            return Get_int_big_num(i,j);
+        }
+
+
+
+
+
     }
 }
